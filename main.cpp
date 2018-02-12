5d774a7fa2d49dc471eaa9fd1f85eeca0905ad03
zhenqiang <xiamenhai@gmail.com>
2018-02-12 14:29:53 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "fun_1.h"
+
+int main(){
+  print_hello();
+  std::cout << std::endl;
+  std::cout << "The factorial is " << factorial(5)<< std::endl;
+  return 0;
+  }
+
